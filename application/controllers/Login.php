b5570dcc6d26fc52f5da6454a0661aac5285880a
Herman-ID <hermansh.id@gmail.com>
2019-05-10 14:54:27 +0700

    initial

---
+++ application/controllers/Login.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    
+	function __construct(){
+		parent::__construct();		
+        if($this->session->userdata('status') == "login"){
+			redirect(base_url("dashboard"));
+        }
+        $this->load->model('m_login');
+    }
+    
+	public function index()
+	{
+        
+        $data['title'] = "Masuk | JAPRI";
+        $data['page'] = "login";
+        $this->load->view('template/content', $data);
+        
+    }
+
+    function aksi_login(){
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$where = array(
+			'username' => $username,
+			'pass' => md5($password)
+			);
+		$cek = $this->m_login->cek_login("admin",$where)->num_rows();
+		if($cek > 0){
+ 
+			$data_session = array(
+				'nama' => $username,
+				'status' => "login"
+				);
+ 
+			$this->session->set_userdata($data_session);
+ 
+			redirect(base_url("dashboard"));
+ 
+		}else{
+			echo "Username dan password salah !";
+		}
+	}
+ 
+	function logout(){
+		$this->session->sess_destroy();
+		redirect(base_url('login'));
+	}
+}
---
+++ application/views/template/content.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $this->view('template/head'); ?>
+
+
+    <?php $this->view('page/'.$page); ?>
+<?php $this->view('template/footer'); ?>
+
+</html>
