51c2c28f5605581c43037a895feafe1a34bcd3c9
Herman-ID <hermansh.id@gmail.com>
2019-05-18 22:02:25 +0700

    Add Label to All Form

--- application/views/page/modal.php
+++ application/views/page/modal.php
@@ -31,48 +31,25 @@
       <form action="<?= base_url('crew/tambah'); ?>" method="post">
         <div class="modal-body">
           <div class="form-group">
+            <label class="col-form-label" for="nama"><b>Nama Crew</b></label>
             <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama">
           </div>
           <div class="form-group">
+              <label class="col-form-label" for="tempat_lahir"><b>Tempat Lahir</b></label>
             <input type="text" class="form-control" id="tempat_lahir" name="tempat_lahir" placeholder="Tempat Lahir">
           </div>
           <div class="form-group">
+              <label class="col-form-label" for="tgl_lahir"><b>Tanggal Lahir</b></label>
             <input type="date" class="form-control" id="tgl_lahir" name="tgl_lahir" placeholder="Tanggal Lahir">
           </div>
           <div class="form-group">
+
+              <label class="col-form-label" for="alamat"><b>Alamat</b></label>
             <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Alamat">
             <!-- input hidden dan memiliki nilai default -->
             <input type="hidden" class="form-control" id="bagihasil" name="bagihasil" value="0">
             <input type="hidden" class="form-control" id="piutang" name="piutang" value="0">
             <input type="hidden" class="form-control" id="keterangan" name="keterangan" value="-">
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="submit" class="btn btn-primary">Tambah</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-
-<!-- Modal Tambah Harga-->
-<div class="modal fade" id="tambahHargaModal" tabindex="-1" role="dialog" aria-labelledby="tambahHargaModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="tambahHargaModalLabel">Tambah Harga</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form action="<?= base_url('harga/tambah'); ?>" method="post">
-        <div class="modal-body">
-          <div class="form-group">
-            <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Barang">
-          </div>
-          <div class="form-group">
-            <input type="number" class="form-control" id="harga" name="harga" placeholder="Harga">
           </div>
         </div>
         <div class="modal-footer">
@@ -97,10 +74,12 @@
       <form action="<?= base_url('diskon/tambah'); ?>" method="post">
         <div class="modal-body">
           <div class="form-group">
+            <label class="col-form-label" for="nama"><b>Nama Diskon</b></label>
             <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Diskon">
           </div>
           <div class="form-group">
-            <input type="number" class="form-control" id="potongan" name="potongan" placeholder="Potongan">
+            <label class="col-form-label" for="potongan"><b>Potongan (%)</b></label>
+            <input type="text" class="form-control" id="potongan" name="potongan" placeholder="Nama Diskon">
           </div>
         </div>
         <div class="modal-footer">
